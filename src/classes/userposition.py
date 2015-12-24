c84dc3327c17f0629c7a291c57c596b38520ffd9
Laura Klünder <laura@codingcatgirl.de>
2015-12-24 15:29:22 +0100

    userposition.py was missing

---
+++ src/classes/userposition.py
@@ -0,0 +1,24 @@
+from .location import Location
+from .position import Position
+
+
+class UserPosition(Position, Location):
+    priority = 3
+
+    def __init__(self, level, x, y):
+        Position.__init__(self, level, x, y)
+        Location.__init__(self, self.subtitle, {})
+
+    @property
+    def title(self):
+        room = self.room if not self.forced else self.room_before
+        if room is not None:
+            return 'Custom location in %s' % self.room.title
+        return 'Custom location on level %d' % self.level
+
+    @property
+    def subtitle(self):
+        return '%d:%d:%d' % (self.level, self.x, self.y)
+
+    def __repr__(self):
+        return 'UserPosition%s' % repr((self.level, self.x, self.y))
